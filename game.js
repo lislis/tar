aecb58d0288bea82515e47764b99a66483840ca4
lislis <mail@lislis.de>
2018-01-27 23:56:36 +0100

    throw in web fonts, collision detect

--- game.js
+++ game.js
@@ -2,9 +2,11 @@ var raf;
 var midi;
 
 var strength = 100;
-var speedFactor = 0.01;
+var score = 0;
+var speedFactor = 0.03;
 
 var cam = document.getElementById('camera');
+var light = document.getElementById('light');
 
 if (navigator.requestMIDIAccess) {
   navigator.requestMIDIAccess({
@@ -15,7 +17,7 @@ if (navigator.requestMIDIAccess) {
 }
 
 function onMIDISuccess(midiAccess) {
-  midi = new MidiMgmt(midiAccess);
+  midi = new MidiMgmt(midiAccess, light);
   midi.lp.reset();
 }
 
@@ -23,11 +25,33 @@ function onMIDIFailure(error) {
   console.log("No access to MIDI devices or your browser doesn't support WebMIDI API. Please use WebMIDIAPIShim " + error);
 }
 
+var boxZ = document.querySelector('.box');
+var scoreBoard =  document.querySelector('#score');
+var strength =  document.querySelector('#hud');
+
 function frame(dt) {
   let pos = cam.getAttribute('position');
   cam.setAttribute('position', {x: pos.x, y: pos.y, z: pos.z - speedFactor } );
-  raf = window.requestAnimationFrame(frame);
   //console.log(Math.floor(dt) % 1000);
+
+  if (cam.getAttribute('position').z <= boxZ.getAttribute('position').z) {
+    console.log(cam);
+    boxZ.parentNode.removeChild(boxZ);
+    score ++;
+    scoreBoard.setAttribute('text', 'value', `Barriers passed: ${score}`);
+
+    // get current light color
+    // get color of barriers
+    // calculate damage to strength
+    // if strength < 0 -> end game
+
+    let newBox = document.querySelector('.box');
+    if (newBox) {
+      boxZ = newBox;
+    } // else end game?
+  }
+
+  raf = window.requestAnimationFrame(frame);
 }
 
 function cancelFrame(id) {
@@ -40,3 +64,7 @@ document.querySelector('#intro button').addEventListener('click', () => {
   frame();
   midi.lp.illuminateGrid();
 });
+
+
+// end screen
+// highscore strength times barriers
